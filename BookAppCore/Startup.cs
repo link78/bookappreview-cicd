b212c6f3fca2d0e03699c1f09f84ce3005824f70
Abdoul Koanda <Abdoul.Koanda@biberk.com>
2022-03-08 11:13:13 -0500

    upgrading to dot net core 5

--- BookAppCore/Startup.cs
+++ BookAppCore/Startup.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using BookAppCore.Models;
 using Microsoft.EntityFrameworkCore;
 using BookAppCore.Abstract;
@@ -16,18 +12,23 @@ namespace BookAppCore
     public class Startup
     {
 
-        public Startup(IHostingEnvironment env)
+        //public Startup(IHostingEnvironment env)
+        //{
+        //    var builder = new ConfigurationBuilder()
+        //        .SetBasePath(env.ContentRootPath)
+        //        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        //        .AddEnvironmentVariables();
+        //    Configuration = builder.Build();
+
+
+        //}
+
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-
-
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -39,21 +40,30 @@ namespace BookAppCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
-            app.UseBrowserLink();
-            app.UseStatusCodePages();
+            app.UseRouting();
 
+            app.UseAuthorization();
 
-            app.UseMvcWithDefaultRoute();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
 
-           // Seeding.EnsuredSeed(app);
+            //app.UseMvcWithDefaultRoute();
+
+            // Seeding.EnsuredSeed(app);
         }
     }
 }
